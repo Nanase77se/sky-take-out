706078e7de69b0c03ddb74df2e2d7a814aff5496
Nanase77se <harukaaa537898@gmail.com>
2026-05-23 10:10:40 +0900

    fix: springfox无法解析springboot新版本api

--- my_dev/sky-server/src/main/java/com/sky/config/WebMvcConfiguration.java
+++ my_dev/sky-server/src/main/java/com/sky/config/WebMvcConfiguration.java
@@ -3,6 +3,7 @@ package com.sky.config;
 import com.sky.interceptor.JwtTokenAdminInterceptor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.config.BeanPostProcessor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
@@ -64,5 +65,32 @@ public class WebMvcConfiguration extends WebMvcConfigurationSupport {
     protected void addResourceHandlers(ResourceHandlerRegistry registry) {
         registry.addResourceHandler("/doc.html").addResourceLocations("classpath:/META-INF/resources/");
         registry.addResourceHandler("/webjars/**").addResourceLocations("classpath:/META-INF/resources/webjars/");
+    }
+
+
+    @Bean
+    public static BeanPostProcessor springfoxHandlerProviderBeanPostProcessor() {
+        return new BeanPostProcessor() {
+            @Override
+            public Object postProcessAfterInitialization(Object bean, String beanName) throws org.springframework.beans.BeansException {
+                if (bean instanceof org.springframework.web.servlet.mvc.method.annotation.RequestMappingHandlerMapping 
+                        || bean instanceof org.springframework.web.servlet.handler.HandlerMappingIntrospector) {
+                    // 苍穹外卖特殊处理：不干涉这两个核心映射，直接返回
+                    return bean;
+                }
+                if (bean instanceof springfox.documentation.spring.web.plugins.WebMvcRequestHandlerProvider) {
+                    try {
+                        java.lang.reflect.Field field = org.springframework.util.ReflectionUtils.findField(bean.getClass(), "handlerMappings");
+                        org.springframework.util.ReflectionUtils.makeAccessible(field);
+                        java.util.List<org.springframework.web.servlet.mvc.method.RequestMappingInfoHandlerMapping> mappings = 
+                                (java.util.List<org.springframework.web.servlet.mvc.method.RequestMappingInfoHandlerMapping>) field.get(bean);
+                        mappings.removeIf(mapping -> mapping.getPatternParser() != null);
+                    } catch (Exception e) {
+                        e.printStackTrace();
+                    }
+                }
+                return bean;
+            }
+        };
     }
 }
